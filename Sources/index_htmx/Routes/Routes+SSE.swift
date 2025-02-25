080aa7e96a2bfbb967e120850b886c38d5cde6cd
Benedek Kozma <cyberbeni@gmail.com>
2025-02-25 20:31:00 +0100

    Add todo

--- Sources/index_htmx/Routes/Routes+SSE.swift
+++ Sources/index_htmx/Routes/Routes+SSE.swift
@@ -14,6 +14,8 @@ extension Router {
 						try await writer.writeSSE(event: "reload", html: nil)
 						try await Task.sleep(for: .seconds(1))
 					} else {
+						// TODO: stream the same event to  everyone
+						// https://github.com/hummingbird-project/hummingbird-examples/blob/bcac6b501ab36f8df8e409d9893fb70921b64ae4/server-sent-events/Sources/App/Application%2Bbuild.swift#L67-L93
 						for await _ in AsyncTimerSequence.repeating(every: .seconds(1)).cancelOnGracefulShutdown() {
 							try await writer.writeSSE(html: TimeElement())
 						}
