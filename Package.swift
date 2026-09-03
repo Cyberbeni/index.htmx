7299ab4190010c9a342dd9c838364e3a662d1ac0
Benedek Kozma <cyberbeni@gmail.com>
2026-09-03 17:04:06 +0200

    Remove unnecessary treat all warnings as error

--- Package.swift
+++ Package.swift
@@ -37,7 +37,6 @@ let package = Package(
 			swiftSettings: [
 				.define("DEBUG", .when(configuration: .debug)),
 				.unsafeFlags(["-Xfrontend", "-warn-long-expression-type-checking=100"], .when(configuration: .debug)),
-				.treatAllWarnings(as: .error, .when(configuration: .release)),
 				// .enableUpcomingFeature("NonisolatedNonsendingByDefault"),
 			],
 			linkerSettings: [
